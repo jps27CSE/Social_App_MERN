1e001f5fda468c1f59933e94ef4965f72e7cb6ce
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2023-06-10 01:08:45 +0600

    namefix

--- frontend/src/scenes/loginPage/index.js
+++ frontend/src/scenes/loginPage/index.js
@@ -14,7 +14,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Facebook
+          Storybook
         </Typography>
       </Box>
 
