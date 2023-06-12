2148df92bdc5ff4df4f47558f4627d72d2560aac
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2023-06-12 20:35:54 +0600

    posting layout

--- frontend/src/scenes/homePage/index.js
+++ frontend/src/scenes/homePage/index.js
@@ -2,11 +2,15 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+// import PostsWidget from "scenes/widgets/PostsWidget";
+// import AdvertWidget from "scenes/widgets/AdvertWidget";
+// import FriendListWidget from "scenes/widgets/FriendListWidget";
+
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
-
-  console.log(picturePath);
 
   return (
     <Box>
@@ -18,12 +22,24 @@ const HomePage = () => {
         gap="0.5rem"
         justifyContent="space-between"
       >
-        <UserWidget userId={_id} picturePath={picturePath} />
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          {/* <PostsWidget userId={_id} /> */}
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            {/* <FriendListWidget userId={_id} /> */}
+          </Box>
+        )}
       </Box>
-      <Box
-        flexBasis={isNonMobileScreens ? "42%" : undefined}
-        mt={isNonMobileScreens ? undefined : "2rem"}
-      ></Box>
     </Box>
   );
 };
